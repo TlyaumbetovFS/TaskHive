dd8cc49cc5ab75349d546475c0a3d6d3fd742802
nevermind <143873526+TlyaumbetovFS@users.noreply.github.com>
2026-06-18 00:23:36 +0500

    fix: remove duplicate @EnableJpaAuditing

--- src/main/java/com/taskhive/config/CacheConfig.java
+++ src/main/java/com/taskhive/config/CacheConfig.java
@@ -2,10 +2,8 @@ package com.taskhive.config;
 
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableCaching
-@EnableJpaAuditing
 public class CacheConfig {
 }
